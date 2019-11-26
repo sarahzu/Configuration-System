6946aa1bbd7dc47575643186a405435bfce1d96f
Sarah Zurmühle <s.zurmuehle@hispeed.ch>
2019-11-26 09:50:34 +0100

    add model tables to sql schema

--- backend/configuration/schema.sql
+++ backend/configuration/schema.sql
@@ -7,6 +7,8 @@ CREATE TABLE configuration (
   config_id INTEGER PRIMARY KEY AUTOINCREMENT
 );
 
+INSERT INTO configuration (config_id) VALUES (1);
+
 CREATE TABLE component (
   component_id INTEGER PRIMARY KEY AUTOINCREMENT,
   config_id INTEGER NOT NULL,
@@ -42,5 +44,40 @@ CREATE TABLE decision_card (
 CREATE TABLE general_settings (
     git_repo_address STRING,
     config_id INTEGER NOT NULL,
-  FOREIGN KEY (config_id) REFERENCES configuration (config_id)
-)
+    FOREIGN KEY (config_id) REFERENCES configuration (config_id)
+);
+
+CREATE TABLE model (
+    model_id  INTEGER PRIMARY KEY AUTOINCREMENT,
+    config_id INTEGER NOT NULL,
+    model_name TEXT UNIQUE NOT NULL,
+    FOREIGN KEY (config_id) REFERENCES configuration (config_id)
+);
+
+CREATE TABLE private_vehicles (
+  model_id INTEGER,
+  model_name TEXT NOT NULL,
+  name TEXT UNIQUE NOT NULL,
+  first_unit_name TEXT,
+  second_unit_name TEXT,
+  first_unit_value TEXT,
+  second_unit_value TEXT,
+  FOREIGN KEY (model_id) REFERENCES model (model_id)
+);
+
+INSERT INTO private_vehicles (
+model_id, model_name, name, first_unit_name, second_unit_name, first_unit_value, second_unit_value)
+VALUES (1, 'Private Vehicles', 'Stock', 'Number of Vehicles', 'Tons of Materials', '1000', '22000');
+
+INSERT INTO model (config_id, model_name) VALUES (1, 'Private Vehicles');
+INSERT INTO model (config_id, model_name) VALUES (1, 'Public Vehicles');
+INSERT INTO model (config_id, model_name) VALUES (1, 'Residential Buildings');
+INSERT INTO model (config_id, model_name) VALUES (1, 'Other Buildings');
+INSERT INTO model (config_id, model_name) VALUES (1, 'Infrastructure');
+INSERT INTO model (config_id, model_name) VALUES (1, 'End of Life Mgmt.');
+INSERT INTO model (config_id, model_name) VALUES (1, 'Industry');
+INSERT INTO model (config_id, model_name) VALUES (1, 'Energy');
+
+
+
+
